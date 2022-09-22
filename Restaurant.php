13774696c6b150a6ee190f92e2b876baf6a22ab4
mrodriguezzsx <113449897+mrodriguezzsx@users.noreply.github.com>
2022-09-22 20:51:48 +0200

    Add files via upload

---
+++ Restaurant.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Pràctica 1</title>
+</head>
+<body>
+    <?php
+    $x = 1;
+    $random = rand(0,5);
+    $arrayTaula = array($random,$random,$random,$random,$random,$random,$random,$random,$random,$random);
+    ?>
+
+    <p></p>
+
+</body>
+</html>
